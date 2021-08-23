01288087c00374df6c8b1f7cf3a55fa553a53fce
unknown <ritikpatel05121999@gmail.com>
2021-08-23 18:24:59 +0530

    refactor: modify description and title of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,78 +1,80 @@
 const projects = [
 	{
-		title: 'Triangles Playground',
+		title: 'M14: Stocks Calculator App',
 		description:
-			'From fun quiz on triangles, finding area of triangles, checking whether triangle is valid or not, to finding hypotenuse of triangle. We got you covered.',
-		live: 'https://trianglesplayground.netlify.app/',
-		github: 'https://github.com/ritik-patel05/triangles-playground',
-	},
-	{
-		title: 'Stocks Calculator',
-		description:
-			'Purchased few stocks, and would like to know if you have earned some profit or made a loss. This calculator is the right choice for you!',
+			'Purchased few stocks, and would like to know if you have earned some profit or made a loss. This app is the right choice for you!',
 		live: 'https://stockscalculator.netlify.app/',
 		github: 'https://github.com/ritik-patel05/stock-profit-or-loss',
 	},
 	{
-		title: 'Birthdate Palindrome',
+		title: 'M13: Birthdate Palindrome App',
 		description:
 			'Wanna see if your birthdate is a palindrome, or by how many days have you missed so that your birthdate would be a palindrome?',
 		live: 'https://birthdateepalindrome.netlify.app/',
 		github: 'https://github.com/ritik-patel05/birthdate-palindrome',
 	},
 	{
-		title: 'Is your Birthday Lucky',
+		title: 'M12:Triangles Playground App',
+		description:
+			'From fun quiz on triangles, finding area of triangles, checking whether triangle is valid or not, to finding hypotenuse of triangle. We got you covered.',
+		live: 'https://trianglesplayground.netlify.app/',
+		github: 'https://github.com/ritik-patel05/triangles-playground',
+	},
+	{
+		title: 'M11: Is your Birthday Lucky App',
 		description:
 			'Find if your birthday was really a lucky one? Check it right now!',
 		live: 'https://isyourbirthdayylucky.netlify.app/',
 		github: 'https://github.com/ritik-patel05/is-your-birthday-lucky',
 	},
 	{
-		title: 'Cash Resgister',
+		title: 'M10: Cash Resgister App',
 		description:
 			'Being a shopkeeper, ever wondered what could be the minimum notes to return to the customer after their payment of the bill? This app is for you!',
 		live: 'https://minimum-notes-return.netlify.app/',
 		github: 'https://github.com/ritik-patel05/cash-register',
 	},
 	{
-		title: 'Activity Emoji Interpreter',
+		title: 'M9: Music Recommendation App',
+		description:
+			'My favourite artists and some of their best songs are displayed, so you can also improve your music taste.',
+		live: 'https://musicrecommend.netlify.app/',
+		github: 'https://github.com/ritik-patel05/music-recommendation',
+	},
+	{
+		title: 'M8: Activity Emoji Interpreter App',
 		description:
 			'We sometimes gets lost, finding true meaning of emoji. Search up an emoji or select from the given ones, to find their meaning.',
 		live: 'https://findemojimeaning.netlify.app/',
 		github: 'https://github.com/ritik-patel05/activity-emoji-explorer',
 	},
+
 	{
-		title: 'Music Recommendation',
+		title: 'M7: The Way Of Pirates App',
 		description:
-			'My favourite artists and some of their best songs are displayed, so you can also improve your music taste xD.',
-		live: 'https://musicrecommend.netlify.app/',
-		github: 'https://github.com/ritik-patel05/music-recommendation',
+			'A translation app for pirate lovers, one eyed fellows. It converts English language to Pirates language.',
+		live: 'https://pirateey.netlify.app/',
+		github: 'https://github.com/ritik-patel05/pirate-translation',
 	},
 	{
-		title: 'Minions Translation',
+		title: 'M6: Minions Translation App',
 		description:
 			'An app that can speak in the words of minions, dont believe me? Check this app that translates our language into code words of minions. Isnt it cool?',
 		live: 'https://minionsspeaks.netlify.app/',
 		github: 'https://github.com/ritik-patel05/banana-speak',
 	},
+
 	{
-		title: 'The Way Of Pirates',
+		title: 'M2: A Quiz on Valorant',
 		description:
-			'An another translation app for pirate lovers, one eyed fellows.',
-		live: 'https://pirateey.netlify.app/',
-		github: 'https://github.com/ritik-patel05/pirate-translation',
-	},
-	{
-		title: 'A Quiz on Valorant',
-		description:
-			'If you have ever played Valorant on PC, you will love to take this quiz. ',
+			'If you have ever played Valorant on PC, you will love to take this fun quiz on CLI App.',
 		live: 'https://replit.com/@ritik-patel05/DoYouKnowMe-Quiz-MarkOne?embed=1&output=1#index.js',
 		github: 'https://github.com/ritik-patel05/quiz-valorant',
 	},
 	{
-		title: 'Do you know me Quiz',
+		title: 'M1: Do you know me Quiz',
 		description:
-			'If you are a friend of mine, and would like to judge if you know me well. Give it a shot.',
+			'If you are a friend of mine, and would like to judge if you know me well. Take the fun quiz on CLI App.',
 		live: 'https://replit.com/@ritik-patel05/Quiz-MarkTwo?embed=1&output=1#index.js',
 		github: 'https://github.com/ritik-patel05/do-you-know-me-quiz',
 	},
