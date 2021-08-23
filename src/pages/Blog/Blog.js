188e4c0424a922e15897edd4e3c1d76827fe6c86
unknown <ritikpatel05121999@gmail.com>
2021-08-23 16:54:38 +0530

    feat: added Blogs page

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
+import './Blog.css';
+
 const Blog = () => {
-	return <div></div>;
+	return (
+		<main>
+			<div className='blogs-container'>
+				<div className='blogs-items'>
+					<h2>Most used git commands</h2>
+					<p>
+						Here are the most used git commands, that everyone
+						should know. It includes all the commands that I used
+						during my 6 months intern as a software engineer at
+						GeeksforGeeks.
+					</p>
+					<a href='https://dev.to/ritik_patel05/most-used-git-commands-9bp'>
+						Read article
+					</a>
+				</div>
+				<div className='blogs-items'>
+					<h2>
+						6 months intern as a Software Engineer, GeeksforGeeks
+					</h2>
+					<p>
+						Here is my blog related to how I got opportunity to
+						intern at GeeksforGeeks during my 3rd year, and all the
+						learnings that happened during this time.
+					</p>
+					<a href='https://dev.to/ritik_patel05/6-months-intern-as-a-software-engineer-geeksforgeeks-2l3k'>
+						Read article
+					</a>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Blog;
