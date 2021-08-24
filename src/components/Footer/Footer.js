b2c20713ccb9562ce7a6164163d52d033feacade
unknown <ritikpatel05121999@gmail.com>
2021-08-24 18:30:27 +0530

    feat: add footer heading

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,12 @@ import './Footer.css';
 const Footer = () => {
 	return (
 		<footer>
-			<SocialIcon url='https://www.linkedin.com/in/ritik-patel-5a61211a4/' />
-			<SocialIcon url='https://github.com/ritik-patel05' />
-			<SocialIcon url='https://www.instagram.com/ritik_patel05/' />
+			<h2>Connect Me On Social Media!</h2>
+			<div>
+				<SocialIcon url='https://www.linkedin.com/in/ritik-patel-5a61211a4/' />
+				<SocialIcon url='https://github.com/ritik-patel05' />
+				<SocialIcon url='https://www.instagram.com/ritik_patel05/' />
+			</div>
 		</footer>
 	);
 };
